a6a8dcdc54130b16dce0a3d6371c5ddb183f17ed
ALatronico <gabant47@gmail.com>
2024-12-02 23:14:10 +0100

    feat: add js functions, css style and minor changes

--- script.js
+++ script.js
@@ -18,24 +18,53 @@ const scontoUnder = 0.8;
 const scontoOver = 0.6;
 const kmPrice = 0.21;
 
+generate.addEventListener('click', handlerClickGenerate)
+
+reset.addEventListener('click', handlerClickReset)
+
 //Functions
 function getRandomNumber(min, max){
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+function handlerClickReset() {
 
+  
+  result.classList.toggle('d-none')
 
-// const prezzoBase = km * prezzoPerKm;
-// let prezzoScontato;
-// if (eta < 18)
-// {
-//   prezzoScontato = prezzoBase * scontoUnder;
-//   console.log("il prezzo del biglietto è €", prezzoScontato.toFixed(2));
-// } else if(eta > 65)
-// {
-//   prezzoScontato = prezzoBase * scontoOver;
-//   console.log("il prezzo del biglietto è €", prezzoScontato.toFixed(2));
-// } else 
-// {
-//   console.log("il prezzo del biglietto è €", prezzoBase.toFixed(2));
-// }
+  name.value = ''
+  km.value = ''
+  eta.value = ''
+ 
+  tName.innerText = ''
+  tOff.innerText = ''
+  tCarr.innerText = ''
+  tCpCode.innerText = ''
+  tPrice.innerText = ''
+}
+
+function handlerClickGenerate() {
+  
+  const prezzoBiglietto = km.value * kmPrice;
+  let prezzoFinale = prezzoBiglietto 
+  let message = 'Prezzo Intero'
+
+    if(eta.value === 'minorenne'){
+      prezzoFinale *= scontoUnder;
+      message = 'Sconto Minorenne'
+    
+    }else if(eta.value === 'over65'){
+        prezzoFinale *= scontoOver;
+        message = 'Sconto Over 65'
+    }
+
+    console.log(prezzoFinale)
+
+    result.classList.toggle('d-none')
+
+    tName.innerHTML = name.value
+    tOff.innerText = message
+    tCarr.innerText = getRandomNumber(1,12)
+    tCpCode.innerText = getRandomNumber(10000,1000000)
+    tPrice.innerText = prezzoFinale.toFixed(2) + '€'
+}
